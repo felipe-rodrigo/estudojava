fc1e542f570bf99b62fdfc4595d21745961b4dc0
Usuário 92 <feliperodrigoespindola@gmail.com>
2023-07-05 10:34:49 -0400

    Projeto Escola RestAPI

---
+++ src/main/java/projeto2/escola/domain/Turma.java
@@ -0,0 +1,38 @@
+package projeto2.escola.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Turma {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+  private String nome;
+  private int numeroAlunos;
+
+
+  public int getId() {
+    return id;
+  }
+  public void setId(int id) {
+    this.id = id;
+  }
+  public String getNome() {
+    return nome;
+  }
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+  public int getNumeroAlunos() {
+    return numeroAlunos;
+  }
+  public void setNumeroAlunos(int numeroAlunos) {
+    this.numeroAlunos = numeroAlunos;
+  }
+
+  
+}
---
+++ src/main/java/projeto2/escola/repository/TurmaRepository.java
@@ -0,0 +1,9 @@
+package projeto2.escola.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import projeto2.escola.domain.Turma;
+
+public interface TurmaRepository extends CrudRepository<Turma,Long>{
+  
+}
